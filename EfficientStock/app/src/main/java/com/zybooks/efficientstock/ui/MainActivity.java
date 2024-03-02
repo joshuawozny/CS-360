a894fd55d18f5f4c32bdfd96bc764379347b1c47
Joshua Wozny <joshua.wozny@snhu.edu>
2024-03-02 11:20:33 -0500

    Update app

---
+++ EfficientStock/app/src/main/java/com/zybooks/efficientstock/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zybooks.efficientstock.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zybooks.efficientstock.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
